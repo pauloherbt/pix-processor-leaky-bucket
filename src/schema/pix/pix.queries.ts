540cd593b940fc602728b730af46245b0a303d3a
peaga <pauloherbert@alu.ufc.br>
2026-02-07 15:34:29 -0300

    refactor: removed old schema def

--- src/schema/pix/pix.queries.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLFieldConfigMap, GraphQLString } from 'graphql';
-
-export const pixQueries: GraphQLFieldConfigMap<any, any> = {
-  hello: {
-    type: GraphQLString,
-    resolve: () => 'Hello World from Pix!',
-  },
-};
